31402642276f18ba0142e7007f199c585ede7f4d
Kuzan19 <levan.kanashyan@gmail.com>
2023-11-09 15:25:01 +0300

    initial commit

--- vendor/urls.py
+++ vendor/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='base_page'),
-    path('menu/<slug:slug_subcategory>', views.subcategory, name='subcategory_page'),
-    path('menu/<slug:slug_category>', views.category, name='category_page'),
+    path('menu/<slug:slug_menu>', views.menu, name='menu_page'),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -7,11 +7,10 @@ def index(request):
     return render(request, 'vendor/index.html')
 
 
-def subcategory(request, slug_subcategory):
-    subcategory = get_object_or_404(SubCategory, slug=slug_subcategory)
-    return render(request, 'vendor/some_menu.html', {'subcategory': subcategory})
+def menu(request, slug_menu):
+    try:
+        menu = get_object_or_404(Category, slug=slug_menu)
+    except Exception:
+        menu = get_object_or_404(SubCategory, slug=slug_menu)
+    return render(request, 'vendor/some_menu.html', {"menu": menu})
 
-
-def category(request, slug_category):
-    category = get_object_or_404(Category, slug=slug_category)
-    return render(request, 'vendor/some_menu.html', {'category': category})
